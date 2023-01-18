96a8702b33f710635428b3013fb8bb00dd657397
RajBusa <20it0112charusat.edu.in>
2023-01-18 21:29:26 +0530

    Regular Expression add in PR1

--- app/src/main/java/com/example/practical1/MainActivity.java
+++ app/src/main/java/com/example/practical1/MainActivity.java
@@ -11,6 +11,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class MainActivity extends AppCompatActivity {
     Button loginbtn;
     EditText emailId, pass;
@@ -27,10 +30,27 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String str1 = String.valueOf(emailId.getText());
                 String str2 = String.valueOf(pass.getText());
-                if(str1.equals("20IT011") && str2.equals("123456789")){
+                String regex = "^(?=.*[0-9])"
+                        + "(?=.*[a-z])(?=.*[A-Z])"
+                        + "(?=.*[@#$%^&+=])"
+                        + "(?=\\S+$).{8,20}$";
+                Pattern p = Pattern.compile(regex);
+                Matcher m = p.matcher(str2);
+                if(str1.isEmpty() || str2.isEmpty()){
+                    Toast.makeText(getApplicationContext(), "Please Enter the Data", Toast.LENGTH_SHORT).show();
+                }
+                if(str1.length() < 5 || str1.length() > 8){
+                    Toast.makeText(getApplicationContext(), "Please Enter username between 5 to 8 char", Toast.LENGTH_SHORT).show();
+                }
+                if(!m.matches()){
+                    Toast.makeText(getApplicationContext(), "Please Enter correct password", Toast.LENGTH_SHORT).show();
+                }
+                if(str1.equals("20IT011") && str2.equals("RAJraj@12345")){
                     Intent i = new Intent(MainActivity.this, activity_home.class);
                     startActivity(i);
                     finish();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please Enter Correct username and password", Toast.LENGTH_SHORT).show();
                 }
             }
         });
