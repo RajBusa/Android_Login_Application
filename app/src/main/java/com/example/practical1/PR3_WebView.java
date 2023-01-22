6f2d4ed2a871832d4b9018780b639931e97c8302
RajBusa <20it0112charusat.edu.in>
2023-01-22 16:37:36 +0530

    Practical3 (WebView) completed

--- app/src/main/java/com/example/practical1/PR3_WebView.java
+++ app/src/main/java/com/example/practical1/PR3_WebView.java
@@ -1,14 +1,58 @@
 package com.example.practical1;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class PR3_WebView extends AppCompatActivity {
-
+    WebView webView;
+    ProgressBar pgBar;
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pr3_web_view);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        webView = findViewById(R.id.webView);
+        pgBar = findViewById(R.id.pgBar);
+        webView.loadUrl("https://www.google.com/");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pgBar.setVisibility(View.VISIBLE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pgBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return super.shouldOverrideUrlLoading(view, request);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
